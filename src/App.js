c45070368fa188a79376dc50aa72f1d60fd538ea
Teresa <107971753+TMortara@users.noreply.github.com>
2023-05-29 19:50:00 -0500

    clean up index.js and app.js by removing commented out code

--- src/App.js
+++ src/App.js
@@ -1,92 +1,46 @@
-// import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-
-import { GlobalStyles } from './Components/styles/Global.styled';
+import { GlobalStyles } from "./Components/styles/Global.styled";
 import { ThemeProvider } from "styled-components";
 
-// Import Pages
-// import AboutMe from './Pages/AboutMe';
-// import Projects from './Pages/Projects';
-// import Resume from './Pages/Resume';
-// import Contact from './Pages/ContactMe';
-
-// // Import Components
-// // import Navbar from './Components/Navbar/Navbar';
-// import Footer from './Components/Footer';
-// import ButtonComponent from "./Components/HelperComponent/Button";
-
-
-// // import PageWrapper from "./Components/HelperComponent/PageWrapper";
-
-// import HeroComponent from './Components/HelperComponent/Hero';
-
 import { NavBar } from "./Components/Navbar/NavBar";
-import { Hero } from './Components/HelperComponent/Hero';
-import { Container } from './Components/styles/layout/Container.styled';
-import { AboutMe } from './Components/Pages/AboutMe';
-import { Projects } from './Components/Pages/Projects';
-import { Contact } from './Components/Pages/ContactMe';
-// import { Resume } from './Components/Pages/Resume';
-import { Foooter } from './Components/HelperComponent/Footer';
-// import { Hero } from './Components/HelperComponent/Hero';
-
-
-// function Layout (props){
-//   return <><GlobalStyles />
-//     <NavBar />
-//     <Outlet />
-
-//     {/* <HeroComponent /> */}
-//     <Footer />
-//   </>
-// }
+import { Hero } from "./Components/HelperComponent/Hero";
+import { Container } from "./Components/styles/layout/Container.styled";
+import { AboutMe } from "./Components/Pages/AboutMe";
+import { Projects } from "./Components/Pages/Projects";
+import { Contact } from "./Components/Pages/ContactMe";
+import { Foooter } from "./Components/HelperComponent/Footer";
 
 const App = () => {
-  
   const theme = {
     colors: {
       white: "rgba(240, 240, 242)",
       darkGold: "rgba(191, 126, 4)",
       gold: "rgb(191,144,4)",
-      // darkGreen: "rgb(38, 38, 37)",
       darKGreen: "rgb(44, 54, 57)",
       green: "rgba(63, 78, 79)",
       salmon: "rgba(242, 152, 99)",
       mauve: "rgba(141,89,90)",
       beige: "rgb(216, 188, 171)",
-      cream: "rgb(220, 215, 201)"
+      cream: "rgb(220, 215, 201)",
     },
     mobile: "768px",
-    transition: "all 400ms ease-in-out"
-  }
+    transition: "all 400ms ease-in-out",
+  };
 
   return (
     <>
-    <ThemeProvider theme = {theme}>
-      <GlobalStyles />
-      <NavBar />
-      <Hero />
-      <Container>
-        <AboutMe />
-        <Projects />
-        {/* <Resume /> */}
-        <Contact />
-        <Foooter />
-      </Container>
-
-    {/* <BrowserRouter>
-     <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route path='aboutme' index element={<AboutMe />}/>
-        <Route path='portfolio' element={<Projects />}/>
-        <Route path='resume' element={<Resume />}/>
-        <Route path='contact' element={<Contact />}/>
-        <Route path='*' element={<h1>Route does not current exist</h1>}/>
-        </Route>
-     </Routes>
-     </BrowserRouter> */}
-     </ThemeProvider>
-     </>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <NavBar />
+        <Hero />
+        <Container>
+          <AboutMe />
+          <Projects />
+          <Contact />
+          <Foooter />
+        </Container>
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-// import AboutMe from './Pages/AboutMe';
-// import Portfolio from './Pages/Portfolio';
-// import Resume from './Pages/Resume';
-// import Contact from './Pages/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
